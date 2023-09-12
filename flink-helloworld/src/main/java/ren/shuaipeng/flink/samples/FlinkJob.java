a8477ac8cf4816baf668949d0f46115696837127
niaoshuai <niao.shuai123@163.com>
2023-09-12 14:42:55 +0800

    init

--- flink-helloworld/src/main/java/ren/shuaipeng/flink/samples/FlinkJob.java
+++ flink-helloworld/src/main/java/ren/shuaipeng/flink/samples/FlinkJob.java
@@ -1,7 +1,5 @@
 package ren.shuaipeng.flink.samples;
 
-import lombok.Data;
-import lombok.experimental.Accessors;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -14,32 +12,37 @@ public class FlinkJob {
 
         // 2. 定义数据流
         DataStream<Person> streamData = env.fromElements(
-                new Person()
-                        .setAge(35)
-                        .setName("三十岁以后"),
-                new Person()
-                        .setAge(18)
-                        .setName("小任"),
-                new Person()
-                        .setAge(2)
-                        .setName("小妞")
+                new Person("三十岁以后", 35),
+                new Person("小任", 18),
+                new Person("小妞", 2)
         );
 
         // 3. 定义 流计算
         DataStream<Person> filterAdults =
                 streamData.filter((FilterFunction<Person>) person -> person.age > 18);
 
-        // 4. 打印 计算结果
+        // 4. 打印 流信息
         filterAdults.print();
 
         // 5. 开启流计算
         env.execute("Stream Calc Adults");
     }
 
-    @Data
-    @Accessors(chain = true)
     private static class Person {
         public String name;
         public Integer age;
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
 }
